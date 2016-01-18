203e4e0f84b8aac246f1fd970957857966803d89
Csaba Valyi <csaba_valyi@epam.com>
2016-01-18 15:33:17 +0100

    basic node server with socket.io created

---
+++ index.js
@@ -0,0 +1,34 @@
+var config = {
+    port: 3000
+}
+
+var express = require('express'),
+    socket = require('socket.io'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app).listen(config.port),
+    io = socket.listen(server);
+
+io.sockets.on('connection', (client) => {
+    console.log(`client connected`);
+
+    client.on('join', handleUserJoin);
+
+    client.on('messages', handleUserMessage);
+
+    function handleUserJoin(name) {
+        client.set('username', name);
+    }
+
+    /**
+     * data: {
+     *      message: 'your message',
+     *      timestamp: new Date()
+     * }
+     */
+    function handleUserMessage(data) {
+        client.get('username', function(err, name) {
+            client.broadcast.emit('chat', `${name}: ${data.message}`);
+        });
+    }
+});
